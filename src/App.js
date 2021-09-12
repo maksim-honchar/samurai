f4b1dc282fe532341fae6b2a569ccdd4455c6bf9
Maksym Honchar <maksim.honchar@gmail.com>
2021-09-12 21:13:25 +0300

    complete lesson #11

--- src/App.js
+++ src/App.js
@@ -27,6 +27,23 @@ const App = () => {
 				<div>
 					<img src="https://wallpaperaccess.com/full/1971765.jpg" alt="beach" />
 				</div>
+				<div>
+					ava + description
+				</div>
+				<div>
+					My posts
+					<div>
+						New post
+					</div>
+					<div>
+						<div>
+							post 1
+						</div>
+						<div>
+							post 2
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
