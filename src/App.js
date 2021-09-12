f84d1e742f4363910f29c7798b76881dc1a1407d
Maksym Honchar <maksim.honchar@gmail.com>
2021-09-12 18:55:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<div className='app-wrapper'>
+			<header className='header'>
+				<img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt="logo" />
+			</header>
+			<nav className='nav'>
+				<div>
+					<a>Profile</a>
+				</div>
+				<div>
+					<a>Messages</a>
+				</div>
+				<div>
+					<a>News</a>
+				</div>
+				<div>
+					<a>Music</a>
+				</div>
+				<div>
+					<a>Settings</a>
+				</div>
+			</nav>
+			<div className='content'>
+				<div>
+					<img src="https://wallpaperaccess.com/full/1971765.jpg" alt="beach" />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default App;
