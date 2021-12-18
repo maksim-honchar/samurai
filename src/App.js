eed69c2941f80b446ccc44878eb74ba0f76a21f6
Maksym Honchar <maksim.honchar@gmail.com>
2021-12-18 11:04:05 +0200

    end of lesson 12

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
 	return (
 		<div className='app-wrapper'>
-			<header className='header'>
-				<img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt="logo" />
-			</header>
-			<nav className='nav'>
-				<div>
-					<a>Profile</a>
-				</div>
-				<div>
-					<a>Messages</a>
-				</div>
-				<div>
-					<a>News</a>
-				</div>
-				<div>
-					<a>Music</a>
-				</div>
-				<div>
-					<a>Settings</a>
-				</div>
-			</nav>
-			<div className='content'>
-				<div>
-					<img src="https://wallpaperaccess.com/full/1971765.jpg" alt="beach" />
-				</div>
-				<div>
-					ava + description
-				</div>
-				<div>
-					My posts
-					<div>
-						New post
-					</div>
-					<div>
-						<div>
-							post 1
-						</div>
-						<div>
-							post 2
-						</div>
-					</div>
-				</div>
-			</div>
+			<Header />
+			<Navbar />
+			<Profile />
 		</div>
 	)
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div>
+				<img src="https://wallpaperaccess.com/full/1971765.jpg" alt="beach" />
+			</div>
+			<div>
+				ava + description
+			</div>
+			<div>
+				My posts
+				<div>
+					New post
+				</div>
+				<div>
+					<div>
+						post 1
+					</div>
+					<div>
+						post 2
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Profile;
