9ca0d132a9a49f8e92eb4ab864d585973cf9b770
Maksym Honchar <maksim.honchar@gmail.com>
2021-12-20 14:12:49 +0200

    end of lesson 18

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,9 +4,9 @@ import s from './Profile.module.css';
 
 const Profile = () => {
 	return (
-		<div className={s.content}>
+		<div>
 			<div>
-				<img className={s.contentImg} src="https://wallpaperaccess.com/full/1971765.jpg" alt="beach" />
+				<img className='contentImg' src="https://wallpaperaccess.com/full/1971765.jpg" alt="beach" />
 			</div>
 			<div>
 				ava + description
