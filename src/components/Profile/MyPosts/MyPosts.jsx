7d20a9205a26f263ced02aa563e8dc3542969250
Maksym Honchar <maksim.honchar@gmail.com>
2021-12-26 15:50:44 +0200

    end of lesson 39

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/state';
+
 
 const MyPosts = (props) => {
 	let postsElements = props.posts.map(p => <Post message={p.message} likeCounts={p.likesCount} />)
@@ -9,13 +11,14 @@ const MyPosts = (props) => {
 
 	let addPost = () => {
 		// props.addPost()
-		props.dispatch({ type: 'ADD-POST' })
+		props.dispatch(addPostActionCreator())
 	}
 
 	let onPostChange = () => {
 		let text = newPostElelment.current.value;
 		// props.updateNewPostText(text)
-		const action = { type: 'UPDATE-NEW-POST-TEXT', newText: text };
+		// const action = { type: 'UPDATE-NEW-POST-TEXT', newText: text };
+		const action = updateNewPostTextActionCreator(text);
 		props.dispatch(action)
 	}
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 const store = {
 	_state: {
 		profilePage: {
@@ -40,7 +43,7 @@ const store = {
 	},
 
 	dispatch(action) {
-		if (action.type === 'ADD-POST') {
+		if (action.type === ADD_POST) {
 			const newPost = {
 				id: 5,
 				message: this._state.profilePage.newPostText,
@@ -49,12 +52,17 @@ const store = {
 			this._state.profilePage.posts.push(newPost)
 			this._state.profilePage.newPostText = ''
 			this._callSubscriber(this._state)
-		} else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+		} else if (action.type === UPDATE_NEW_POST_TEXT) {
 			this._state.profilePage.newPostText = action.newText;
 			this._callSubscriber(this._state)
 		}
 	}
 }
+
+export const addPostActionCreator = () => ({ type: ADD_POST })
+
+export const updateNewPostTextActionCreator = (text) =>
+	({ type: UPDATE_NEW_POST_TEXT, newText: text })
 
 
 export default store;
