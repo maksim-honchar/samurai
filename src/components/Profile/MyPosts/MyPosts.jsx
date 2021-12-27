c4cc6b2b6705663ffa9719409f8020bbbc9de4ba
Maksym Honchar <maksim.honchar@gmail.com>
2021-12-27 15:20:09 +0200

    end of lesson 41

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/profile-reducer';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/state';
 
 
 const MyPosts = (props) => {
