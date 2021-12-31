8da0d38714979f4c5d199a7bb9d6acd43087275d
Maksym Honchar <maksim.honchar@gmail.com>
2021-12-31 10:49:45 +0200

    end of lesson 42

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './redux/state';
+import store from './redux/redux-store';
 
 const rerenderEntireTree = (state) => {
 	ReactDOM.render(
@@ -17,7 +17,10 @@ const rerenderEntireTree = (state) => {
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree);
+store.subscribe(() => {
+	const state = store.getState()
+	rerenderEntireTree(state)
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,8 +1,27 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
 const SEND_MESSAGE = 'SEND_MESSAGE';
 
+const initialState = {
+	dialogs: [
+		{ id: 1, name: "Dimych" },
+		{ id: 2, name: "Andrey" },
+		{ id: 3, name: "Sveta" },
+		{ id: 4, name: "Sasha" },
+		{ id: 5, name: "Viktor" },
+		{ id: 6, name: "Valera" },
+	],
+	messages: [
+		{ id: 1, message: "Hi!" },
+		{ id: 2, message: "How are you?" },
+		{ id: 3, message: "You!" },
+		{ id: 4, message: "You!" },
+		{ id: 5, message: "You!" },
+	],
+	newMessageBody: ''
+}
 
-const dialogsReducer = (state, action) => {
+
+const dialogsReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case UPDATE_NEW_MESSAGE_BODY:
 			state.newMessageBody = action.body;
