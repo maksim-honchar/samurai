4469a181581e0d485f15a5d3687b3289a981af04
Maksym Honchar <maksim.honchar@gmail.com>
2022-01-30 18:21:36 +0200

    end of lesson 50

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import DialogsContainer from './components/Dialogs/DialogsContainer';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import UsersContainer from './components/users/UsersContainer';
+import UsersContainer from './components/Users/UsersContainer'
 
 const App = (props) => {
 
