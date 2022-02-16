0430efe216b503364c92b8faa7a6e9bcc5547b2c
Maksym Honchar <maksim.honchar@gmail.com>
2022-02-16 13:19:41 +0200

    end of lesson 60

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/profile-reducer';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 
 const MyPosts = (props) => {
-	const postsElements = props.posts.map(p => <Post message={p.message} likeCounts={p.likesCount} />)
+	const postsElements = props.posts.map(p => <Post message={p.message} likeCounts={p.likesCount} key={p.id} />)
 
 	const newPostElelment = React.createRef();
 
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import axios from 'axios'
 import { setUserProfile } from '../../redux/profile-reducer'
@@ -7,8 +8,12 @@ import Profile from './Profile'
 
 class ProfileContainer extends Component {
 	componentDidMount() {
+		let userId = this.props.match.params.userId
+		if (!userId) {
+			userId = 2
+		}
 
-		axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+		axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
 			.then(response => {
 				this.props.setUserProfile(response.data)
 			})
@@ -25,4 +30,6 @@ const mapStateToProps = state => ({
 	profile: state.profilePage.profile
 })
 
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer)
+const WithUrlDataContainerComponent = withRouter(ProfileContainer)
+
+export default connect(mapStateToProps, { setUserProfile })(WithUrlDataContainerComponent)
