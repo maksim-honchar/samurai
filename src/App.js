a7b1eae81ad15d9aaf68915d15a50aa3add5f802
Maksym Honchar <maksim.honchar@gmail.com>
2022-02-20 16:32:40 +0200

    end of lesson 61

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from "react-router-dom";
 import './App.css';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
-import Header from './components/Header/Header';
+import HeaderContainer from './components/Header/HeaderContainer';
 import Navbar from './components/Navbar/Navbar';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import UsersContainer from './components/Users/UsersContainer'
@@ -11,7 +11,7 @@ const App = (props) => {
 
 	return (
 		<div className='app-wrapper'>
-			<Header />
+			<HeaderContainer />
 			<Navbar />
 			<div className='app-wrapper-content'>
 				<Route path='/dialogs'
