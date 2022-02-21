8b2b2987775bad554232833fb071158d3d7a922d
Maksym Honchar <maksim.honchar@gmail.com>
2022-02-21 20:07:03 +0200

    end of lesson 62

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import userPhoto from '../../assets/images/user.png'
@@ -41,8 +42,40 @@ const Users = props => {
 							<div>
 								{
 									u.followed
-										? <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
-										: <button onClick={() => props.follow(u.id)}>Follow</button>
+										?
+										<button onClick={() => {
+											axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+												withCredentials: true,
+												headers: {
+													"API-KEY": "7374640a-3837-40a5-9bac-b674866abc6c"
+												}
+											})
+												.then(response => {
+													if (response.data.resultCode === 0) {
+														props.unfollow(u.id)
+													}
+												})
+										}}
+										>
+											Unfollow
+										</button>
+										:
+										<button onClick={() => {
+											axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+												withCredentials: true,
+												headers: {
+													"API-KEY": "7374640a-3837-40a5-9bac-b674866abc6c"
+												}
+											})
+												.then(response => {
+													if (response.data.resultCode === 0) {
+														props.follow(u.id)
+													}
+												})
+										}}
+										>
+											Follow
+										</button>
 								}
 							</div>
 						</span>
